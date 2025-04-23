8944fd9819c78904a4d24e3513778ebb4322ad37
Fábio Maiorano <fsmaiorano@gmail.com>
2025-04-23 23:24:30 +0100

    test: enhance PayableService tests to include createPayable functionality

--- src/payable/payable.service.spec.ts
+++ src/payable/payable.service.spec.ts
@@ -2,12 +2,14 @@ import { TestingModule, Test } from '@nestjs/testing';
 import { AssignorService } from '../assignor/assignor.service';
 import { PayableService } from './payable.service';
 import { PayableRepository } from './payable.repository';
+import Decimal from 'decimal.js';
 
 describe('PayableService', () => {
   let service: PayableService;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
       providers: [
         PayableService,
         {
@@ -31,5 +33,40 @@ describe('PayableService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a payable', async () => {
+    const assignorService = module.get<AssignorService>(AssignorService);
+    jest.spyOn(assignorService, 'verifyExists').mockResolvedValue(true);
+
+    const payableRepository = module.get<PayableRepository>(PayableRepository);
+    jest.spyOn(payableRepository, 'create').mockResolvedValue({
+      id: '1',
+      value: new Decimal(100),
+      assignorId: '123',
+      emissionDate: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const createPayableRequest = {
+      assignorId: '123',
+      value: new Decimal(100),
+      emissionDate: new Date(),
+    };
+
+    const result = await service.createPayable(createPayableRequest);
+
+    expect(result).toEqual({
+      id: '1',
+      ...createPayableRequest,
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    });
+
+    expect(assignorService.verifyExists).toHaveBeenCalledWith(
+      createPayableRequest.assignorId,
+    );
+    expect(payableRepository.create).toHaveBeenCalled();
   });
 });
