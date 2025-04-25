425a247d92a1f635ed7bd7918787fd427c241927
Fábio Maiorano <fsmaiorano@gmail.com>
2025-04-25 02:07:35 +0100

    feat: update environment configurations and add RabbitMQ client setup

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
   controllers: [],
   providers: [],
   exports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ClientsModule.register([
+      {
+        name: 'ReceivablesFlow',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'receivables_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
 })
 export class SharedModule {}
