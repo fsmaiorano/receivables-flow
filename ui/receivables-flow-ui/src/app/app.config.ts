eb642cea4555d99855aa806e22d274118b8716d5
Fábio Maiorano <fsmaiorano@gmail.com>
2025-05-04 15:07:15 +0100

    feat(app): add animations and native date adapter providers to app configuration

--- ui/receivables-flow-ui/src/app/app.config.ts
+++ ui/receivables-flow-ui/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { routes } from './app.routes';
 import {
@@ -14,5 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
+    provideAnimations(),
+    provideNativeDateAdapter(),
   ],
 };
